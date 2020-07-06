e3c1e48c5dff79806053cf803ad6db7652babb4d
MrSamehOmr <67664393+MrSamehOmr@users.noreply.github.com>
2020-07-07 01:49:18 +0300

    Add files via upload

---
+++ Console WriteLine.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_WriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1234567);
+            Console.WriteLine("Hi Sameh");
+        }
+    }
+}
